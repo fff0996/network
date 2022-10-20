c94ff82c3e0582d9937ae62d2d27204b68d42c1a
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-10-20 10:07:23 +0900

    Create circos.R

---
+++ circos.R
@@ -0,0 +1,7 @@
+pdf("파일이름.pdf",width=15,height=20)
+chordDiagram(as.data.frame(d3), transparency = 0.5,annotationTrack="grid")
+circos.track(track.index = 1, panel.fun = function(x, y) {
+     circos.text(CELL_META$xcenter, CELL_META$ylim[1], CELL_META$sector.index, 
+         facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+ }, bg.border = NA) 
+dev.off()

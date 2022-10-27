d1373ca095616c3138b4550c4bf6d700f8aee4a5
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2022-10-27 10:17:19 +0900

    Update ggplot.R

--- ggplot.R
+++ ggplot.R
@@ -54,3 +54,9 @@ ggplot(data=r,aes(x=r)) + geom_bar()+ theme(axis.text.x = element_text(angle = 4
 
 ggplot(d,aes(x=Category,y=Count)) + geom_bar(stat="identity") + geom_text(aes(label=Count),vjust=0) +theme(axis.text.x = element_text(angle = 45, hjust = 1, size = 10))
 ggplot(SDRatio_simu,aes(x=Group,y=SDRatio)) + geom_bar(stat="identity")  + labs(x="",y="") + ylim(0,20) + scale_x_discrete(limits= c("Heteroscedasticity","Middle Heteroscedasticity","Low Heteroscedasticity","Homoscedasticity"))
+#SDRatio_simu
+#                      Group  SDRatio
+#1        Heteroscedasticity 19.26299
+#2 Middle Heteroscedasticity  4.13090
+#3    Low Heteroscedasticity  1.94864
+#4          Homoscedasticity  1.14711

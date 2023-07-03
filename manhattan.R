2681ff4ec29e8ac4d6a751a3895e5d5ca084ce24
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2023-07-03 17:10:40 +0900

    Update manhattan.R

--- manhattan.R
+++ manhattan.R
@@ -11,3 +11,30 @@ library(qqman)
 
 
 manhattan(notcommongwas,genomewideline= -log10(6.36845e-07),suggestiveline=FALSE)
+
+
+#customize
+don <- notcommongwas %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>% 
+mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>%
+left_join(notcommongwas,.,by=c("CHR"="CHR")) %>% 
+arrange(CHR,BP) %>% 
+mutate(Disease = BP+tot)
+
+
+axisdf = don %>% group_by(CHR) %>% summarize(center=(max(Disease) + min(Disease))/2)
+
+
+ggplot(don,aes(x=Disease,y=-log10(P)))+
+geom_point(aes(color=as.factor(CHR)),alpha=0.8,size=1.3) + 
+scale_color_manual(values=rep(c("grey","black"),22))+
+scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
+scale_y_continuous(expand = c(0, 0) ) +     # remove space between plot area and x axis
+  
+# Custom the theme:
+     theme_bw() +
+    theme( 
+       legend.position="none",
+       panel.border = element_blank(),
+       panel.grid.major.x = element_blank(),
+     panel.grid.minor.x = element_blank()
+     )

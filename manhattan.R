ab2ef67a958d90ed2d4899e3a735daa5f1ad943a
JungHyeIn <54706837+fff0996@users.noreply.github.com>
2023-07-05 16:44:18 +0900

    Update manhattan.R

--- manhattan.R
+++ manhattan.R
@@ -18,18 +18,26 @@ don <- notcommongwas %>% group_by(CHR) %>% summarise(chr_len=max(BP)) %>%
 mutate(tot=cumsum(chr_len)-chr_len) %>% select(-chr_len) %>%
 left_join(notcommongwas,.,by=c("CHR"="CHR")) %>% 
 arrange(CHR,BP) %>% 
-mutate(Disease = BP+tot)
+mutate(Disease = BP+tot) %>%
+
+mutate( is_highlight=ifelse(SNP %in% hl, "yes", "no")) %>%
+#mutate( is_annotate=ifelse(-log10(P)>4, "yes", "no")) 
 
 
 axisdf = don %>% group_by(CHR) %>% summarize(center=(max(Disease) + min(Disease))/2)
 
 
 ggplot(don,aes(x=Disease,y=-log10(P)))+
+               #shape=rfchapter))+
 geom_point(aes(color=as.factor(CHR)),alpha=0.8,size=1.3) + 
-scale_color_manual(values=rep(c("grey","black"),22))+
+scale_color_manual(values=rep(c("grey","black"),20))+
 scale_x_continuous( label = axisdf$CHR, breaks= axisdf$center ) +
 scale_y_continuous(expand = c(0, 0) ) +     # remove space between plot area and x axis
-  
+geom_abline(intercept=-log10(6.36845e-07),slope=0,colour="red") +
+#scale_shape_manual(values=c(15,16,17,18,21,22,23,24,37,0,1,2,3))+
+geom_point(data=subset(don, is_highlight=="yes"), color="orange", size=2) +
+
+
 # Custom the theme:
      theme_bw() +
     theme( 
